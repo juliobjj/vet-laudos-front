2ec10ad6d4dae2747b31c66b87a6d158a3115fe8
Júlio Henrique Rodrigues <julio.rodrigues@showtecnologia.com>
2025-08-30 01:43:40 -0300

    resolvi problema no merge.

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,6 +4,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL: "http://localhost:3000",
   headers: {
-    "Content-Type": "application/json",
+    "Content-Type": "application/json", 
   },
 });
